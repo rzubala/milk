e2701771658b5c8f4397982110765bf5899368c5
Rafał Zubala <rzubala@gmail.com>
2020-05-26 08:10:54 +0200

    icons

--- components/FeedingItem.tsx
+++ components/FeedingItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
   View,
   Text,
+  Image,
   StyleSheet,
   Platform,
   TouchableNativeFeedback,
@@ -23,8 +24,26 @@ const FeedingItem = (props) => {
         <TouchableComponent onPress={props.onSelect} useForeground>
           <View style={styles.feedingRow}>
             <Text style={styles.date}>{props.date}</Text>
-            <Text style={styles.volume}>{props.volume}</Text>
-            {props.count && <Text style={styles.count}>{props.count}</Text> }
+            <View style={styles.imageContainer}>
+              <Image
+                  source={require("../assets/icon.png")}
+                  fadeDuration={0}
+                  style={styles.bottle}
+              />
+              <Text style={styles.volume}>{props.volume}</Text>
+            </View>
+            {props.count && (
+              <View style={styles.imageContainer}>
+                <Image
+                  source={require("../assets/diapers.png")}
+                  fadeDuration={0}
+                  style={styles.diapers}
+                />
+                <View style={styles.countContainer}>
+                  <Text style={styles.count}>{props.count}</Text>
+                </View>
+              </View>
+            )}
           </View>
         </TouchableComponent>
       </View>
@@ -38,25 +57,46 @@ const styles = StyleSheet.create({
     height: 70,
   },
   touchable: {
-    overflow: "hidden"
+    overflow: "hidden",
   },
   feedingRow: {
     flexDirection: "row",
-    justifyContent: 'space-evenly',
+    justifyContent: "space-evenly",
     alignItems: "center",
-    height: '100%'
+    height: "100%",
   },
   date: {
-    fontSize: 15
+    fontSize: 15,
   },
   volume: {
     fontSize: 18,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   count: {
     fontSize: 15,
-    color: Colors.accent
-  }
+    color: Colors.accent,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  diapers: {
+    width: 40, 
+    height: 40
+  },
+  bottle: {
+    width: 40, 
+    height: 40
+  },
+  countContainer: {
+    position: "absolute",
+    top: -10,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default FeedingItem;
