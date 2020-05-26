acf975f3b5f9a402b74090809058bb5d610cf25a
Rafał Zubala <rzubala@gmail.com>
2020-05-26 08:34:01 +0200

    refresh

--- screen/FeedingOverview.tsx
+++ screen/FeedingOverview.tsx
@@ -1,55 +1,53 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { View, Text, StyleSheet, FlatList, Platform, TouchableNativeFeedback, TouchableOpacity, Button, ActivityIndicator } from "react-native";
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  Platform,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import { Colors } from "../constants/colors";
-import { Ionicons } from "@expo/vector-icons";
 import i18n from "../constants/strings";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../components/UI/HeaderButton";
 
 import Feeding from "../domain/feeding";
-import Poo from '../domain/poo'
+import Poo from "../domain/poo";
 import FeedingItem from "../components/FeedingItem";
 import * as feedingActions from "../store/actions/milk";
-import * as pooActions from '../store/actions/poo'
-import * as feedingUtils from '../utils/milk'
+import * as pooActions from "../store/actions/poo";
+import * as feedingUtils from "../utils/milk";
 
 const FeedingOverview = (props) => {
-  const TouchableComponent: any =
-    Platform.OS === "android" && Platform.Version >= 21
-      ? TouchableNativeFeedback
-      : TouchableOpacity;
-
-  const feeding = useSelector((state) => feedingUtils.groupPerDay(state.milk.feeding));
-  const poo = useSelector(state => state.poo.poo)
+  const feeding = useSelector((state) =>
+    feedingUtils.groupPerDay(state.milk.feeding)
+  );
+  const poo = useSelector((state) => state.poo.poo);
+  const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
 
-  const loadFeeding = useCallback(() => {
-    dispatch(feedingActions.fetchFeeding());
-    dispatch(pooActions.fetchPoo());
+  const loadData = useCallback(async () => {
+    try {
+      setLoading(true)
+      await dispatch(feedingActions.fetchFeeding());
+      await dispatch(pooActions.fetchPoo());
+    } catch (err) {      
+    } finally {
+      setLoading(false)
+    }
+
   }, [dispatch]);
 
-  const loadPoo = useCallback(() => {
-    dispatch(pooActions.fetchPoo());
-  }, [dispatch]);  
-
   useEffect(() => {
-    loadFeeding();
-  }, [loadFeeding]);
-
-  useEffect(() => {
-    loadPoo();
-  }, [loadPoo]);
-
-  const tryAgain = () => {
-    loadFeeding()
-    loadPoo()
-  }
+    loadData();
+  }, [loadData]);
 
   const onAdd = useCallback(() => {
-      props.navigation.navigate("FeedingEdit")
+    props.navigation.navigate("FeedingEdit");
   }, []);
 
   useEffect(() => {
@@ -57,10 +55,14 @@ const FeedingOverview = (props) => {
       headerRight: () => (
         <HeaderButtons HeaderButtonComponent={HeaderButton}>
           <Item
+            title="Reload"
+            iconName={Platform.OS === "android" ? "md-refresh" : "ios-refresh"}
+            onPress={loadData}
+            color={Colors.accent}
+          />
+          <Item
             title="Add"
-            iconName={
-              Platform.OS === "android" ? "md-add" : "ios-add"
-            }
+            iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
             onPress={onAdd}
             color={Colors.accent}
           />
@@ -70,23 +72,33 @@ const FeedingOverview = (props) => {
   }, [onAdd]);
 
   const onItemSelected = (date: Date) => {
-    let pooObj: Poo | undefined = feedingUtils.getPoo(poo, date.getTime())
+    let pooObj: Poo | undefined = feedingUtils.getPoo(poo, date.getTime());
     if (!pooObj) {
-      pooObj = new Poo('', date.getTime(), 0)
+      pooObj = new Poo("", date.getTime(), 0);
     }
     props.navigation.navigate("FeedingDayOverview", {
       timestamp: date.getTime(),
-      poo: pooObj
+      poo: pooObj,
     });
   };
 
-  if (!feeding || feeding.length === 0) {
+  if (loading || !feeding || feeding.length === 0) {
     return (
-      <View style={{ ...styles.screen, justifyContent: "center", alignItems: 'center' }}>
+      <View
+        style={{
+          ...styles.screen,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <ActivityIndicator size="large" color={Colors.primary} />
-        <View style={{width: "50%", marginTop: 40}}>
-          <Button title={i18n.t("TryAgain")} onPress={tryAgain} color={Colors.accent} />
-        </View>
+        {!loading && <View style={{ width: "50%", marginTop: 40 }}>
+          <Button
+            title={i18n.t("TryAgain")}
+            onPress={loadData}
+            color={Colors.accent}
+          />
+        </View>}
       </View>
     );
   }
@@ -101,9 +113,13 @@ const FeedingOverview = (props) => {
             <FeedingItem
               date={itemData.item.date.toISOString().slice(0, 10)}
               volume={itemData.item.volume}
-              count={feedingUtils.getPoo(poo, itemData.item.date.getTime())?.count.toString() ?? "0"}
+              count={
+                feedingUtils
+                  .getPoo(poo, itemData.item.date.getTime())
+                  ?.count.toString() ?? "0"
+              }
               onSelect={() => {
-                onItemSelected(itemData.item.date)
+                onItemSelected(itemData.item.date);
               }}
             ></FeedingItem>
           );
@@ -116,7 +132,7 @@ const FeedingOverview = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-  }
+  },
 });
 
 export const screenOptions = () => {
